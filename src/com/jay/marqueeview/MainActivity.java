3a84418d2797c2407099a6af3d46c0c750909634
jay <jayliu1989@live.cn>
2016-06-19 18:44:22 +0800

    1，添加博客地址

--- src/com/jay/marqueeview/MainActivity.java
+++ src/com/jay/marqueeview/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends Activity {
 		List<String> info = new ArrayList<String>();
         info.add("1. 淘宝头条");
         info.add("2. 京东快报");
-        info.add("3. 博客地址：");
+        info.add("3. 博客地址：http://blog.csdn.net/jayliu1989/article/details/51713420");
         info.add("4. 项目地址：https://github.com/Winfred1989/MarqueeView");
         info.add("5. 参考：http://www.jianshu.com/p/d9442041743f");
         mv_list.startWithList(info);
